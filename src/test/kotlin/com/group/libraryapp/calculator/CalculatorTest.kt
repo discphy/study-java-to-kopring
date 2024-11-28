eae24e0ea87457a2576526166e93610e881a7d4c
discphy <dinophy@nate.com>
2024-11-29 08:46:17 +0900

    :sparkles: 계산기 테스트 예외 코드 작성

--- src/test/kotlin/com/group/libraryapp/calculator/CalculatorTest.kt
+++ src/test/kotlin/com/group/libraryapp/calculator/CalculatorTest.kt
@@ -1,9 +1,26 @@
 package com.group.libraryapp.calculator
 
-import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.AssertionsForInterfaceTypes.assertThat
+import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 
 class CalculatorTest {
+
+    companion object {
+        @BeforeAll
+        @JvmStatic
+        fun beforeAll() {
+            println("before all")
+        }
+
+        @AfterAll
+        @JvmStatic
+        fun afterAll() {
+            println("after all")
+        }
+    }
 
     @Test
     fun addTest() {
@@ -39,5 +56,30 @@ class CalculatorTest {
 
         // then
         assertThat(calculator.number).isEqualTo(15)
+    }
+
+    @Test
+    fun divideTest() {
+        // given
+        val calculator = Calculator(5)
+
+        // when
+        calculator.divide(3)
+
+        // then
+        assertThat(calculator.number).isEqualTo(1)
+    }
+
+    @Test
+    fun divideExceptionTest() {
+        // given
+        val calculator = Calculator(5)
+
+        // when & then
+        assertThrows<IllegalArgumentException> {
+            calculator.divide(0)
+        }.apply {
+            assertThat(message).isEqualTo("0으로 나눌 수 없습니다")
+        }
     }
 }
