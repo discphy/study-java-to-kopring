93840e0db3d4fea923594cc891e765bc903b0e7d
discphy <dinophy@nate.com>
2024-12-13 08:46:19 +0900

    :recycle: 클린 코드

--- src/main/kotlin/com/group/libraryapp/domain/user/loanhistory/UserLoanHistoryRepository.kt
+++ src/main/kotlin/com/group/libraryapp/domain/user/loanhistory/UserLoanHistoryRepository.kt
@@ -6,7 +6,5 @@ interface UserLoanHistoryRepository : JpaRepository<UserLoanHistory, Long> {
 
     fun findByBookNameAndStatus(bookName: String, status: UserLoanStatus): UserLoanHistory?
 
-    fun findByStatus(status: UserLoanStatus): List<UserLoanHistory>
-
     fun countByStatus(status: UserLoanStatus): Long
 }
