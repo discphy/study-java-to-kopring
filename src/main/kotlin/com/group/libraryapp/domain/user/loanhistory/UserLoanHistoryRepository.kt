844cf798905ccf913f49a77bdc53cf0a9d6ceb28
discphy <dinophy@nate.com>
2024-12-17 08:37:11 +0900

    :sparkles: UserLoanHistoryRepository Querydsl 적용

--- src/main/kotlin/com/group/libraryapp/domain/user/loanhistory/UserLoanHistoryRepository.kt
+++ src/main/kotlin/com/group/libraryapp/domain/user/loanhistory/UserLoanHistoryRepository.kt
@@ -4,7 +4,4 @@ import org.springframework.data.jpa.repository.JpaRepository
 
 interface UserLoanHistoryRepository : JpaRepository<UserLoanHistory, Long> {
 
-    fun findByBookNameAndStatus(bookName: String, status: UserLoanStatus): UserLoanHistory?
-
-    fun countByStatus(status: UserLoanStatus): Long
 }
